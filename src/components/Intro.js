66f0bce45b70ccbc461f597e48fdde6864e31c2f
Saadiya Moid <saadiyamoid@gmail.com>
2019-10-16 12:12:11 +0100

    New Stuff

---
+++ src/components/Intro.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class Intro extends React.Component {
+
+    render (){
+        return (
+           <p>Welcome to my new App</p>
+        );
+    }
+}
+
+
+export default Intro;
